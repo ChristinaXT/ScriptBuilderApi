bac55a3088f469cdccac7816cfdc5ddeb4f445dd
Guess <guess@Guesss-iMac.local>
2019-09-29 14:24:57 -0400

    added story form wrapper edit

---
+++ client/src/components/EditStoryFormWrapper.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import StoryForm from './StoryForm'
+import { updateStory, deleteStory } from '../actions/myStories'
+import { setFormDataForEdit, resetStoryForm } from '../actions/storyForm'
+import { connect } from 'react-redux'
+
+class EditStoryFormWrapper extends React.Component {
+  componentDidMount(){
+    this.props.story && this.props.setFormDataForEdit(this.props.story)
+  }
+
+  componentDidUpdate(prevProps) {
+    this.props.story && !prevProps.story && this.props.setFormDataForEdit(this.props.story)
+  }
+
+  componentWillUnmount() {
+    this.props.resetStoryForm()
+  }
+
+  handleSubmit = (formData) => {
+    const { updateStory, story, history } = this.props
+    updateStory({
+      ...formData,
+      storyId: story.id
+    }, history)
+  }
+
+  render() {
+    const { history, deleteStory, story } = this.props
+    const storyId = story ? story.id : null
+    return  <>
+              <StoryForm editMode handleSubmit={this.handleSubmit} />
+              <br/>
+              <button style={{color: "red"}} onClick={()=>deleteStory(storyId, history)}>Delete this Story</button>
+            </>
+  }
+};
+
+export default connect(null, { updateStory, setFormDataForEdit, resetStoryForm, deleteStory })(EditStoryFormWrapper);
