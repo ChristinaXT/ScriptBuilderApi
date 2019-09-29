d8af79c92cb1c50ecb907e5e7729ff8dc9ead2c9
Guess <guess@Guesss-iMac.local>
2019-09-29 14:11:08 -0400

    added story form reducer

---
+++ client/src/reducers/storyForm.js
@@ -0,0 +1,21 @@
+const initialState = {
+  name: "",
+
+}
+
+export default (state=initialState, action) => {
+  switch (action.type) {
+    case "UPDATE_NEW_STORY_FORM":
+      const returnVal = {
+        ...state,
+        [action.formData.name]: action.formData.value
+      }
+      return returnVal
+    case "RESET_NEW_STORY_FORM":
+      return initialState
+    case "SET_FORM_DATA_FOR_EDIT":
+      return action.storyFormData
+    default:
+      return state
+  }
+}
