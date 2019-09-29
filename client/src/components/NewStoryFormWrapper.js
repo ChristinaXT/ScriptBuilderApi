5d7e2436243b5fb0d38a7c2cadf626561fe2f487
Guess <guess@Guesss-iMac.local>
2019-09-29 14:27:07 -0400

    added new story form wrapper

---
+++ client/src/components/NewStoryFormWrapper.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import StoryForm from './StoryForm'
+import { createStory } from '../actions/myStories'
+import { connect } from 'react-redux'
+
+const NewStoryFormWrapper = ({ history, createStory }) => {
+
+  const handleSubmit = (formData, userId) => {
+    createStory({
+      ...formData,
+      userId
+    }, history)
+  }
+  return  <StoryForm history={history} handleSubmit={handleSubmit} />
+};
+
+export default connect(null, { createStory })(NewStoryFormWrapper);
