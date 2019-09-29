59c8df7ae8db350b11a9940d53d5aa53478ce201
Guess <guess@Guesss-iMac.local>
2019-09-28 20:54:16 -0400

    added app controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -52,6 +52,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :username, :password_digest)
+      params.require(:user).permit(:name, :username, :password)
     end
 end
