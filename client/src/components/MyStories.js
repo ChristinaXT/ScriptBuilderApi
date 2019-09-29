3e2e5c398d9e2d107323c682e821f63443a29db1
Guess <guess@Guesss-iMac.local>
2019-09-29 13:52:15 -0400

    my stories component

---
+++ client/src/components/MyStories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const MyStories = props => {
+  const storyCards = props.stories.length > 0 ?
+    props.stories.map(t => (<p key={t.id}><Link to={`/stories/${t.id}`}>{t.attributes.name}</Link></p>)) :
+    null
+
+  return storyCards
+}
+
+// we provide mapStateToProps to Redux in order to tell Redux:
+// "Excuse me Redux, would you please provide use access to your state
+// so that we may pick and choose the pieces of state we would like availble
+// to this particular component as props."
+
+const mapStateToProps = state => {
+  return {
+    stories: state.myStories
+  }
+}
+
+export default connect(mapStateToProps)(MyStories)
