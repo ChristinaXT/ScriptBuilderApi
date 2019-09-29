fbb2a42bbded51f4588512efc4dbfa5cd4dcc8c6
Guess <guess@Guesss-iMac.local>
2019-09-29 13:45:38 -0400

    added main container component

---
+++ client/src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyStories from './MyStories.js'
+
+const MainContainer = () => {
+  return (
+    <div className="MainContainer">
+      <MyStories/>
+    </div>
+  )
+}
+
+export default MainContainer
