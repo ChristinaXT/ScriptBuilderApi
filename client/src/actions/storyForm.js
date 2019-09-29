8074a66cb7559e306b51fd029e3545241ad1279d
Guess <guess@Guesss-iMac.local>
2019-09-29 14:04:28 -0400

    added story form action

---
+++ client/src/actions/storyForm.js
@@ -0,0 +1,26 @@
+// sync actions
+
+export const updateStoryForm = (name, value) => {
+  const formData = { name, value }
+  return {
+    type: "UPDATE_NEW_STORY_FORM",
+    formData
+  }
+}
+
+export const resetStoryForm = () => {
+  return {
+    type: "RESET_NEW_STORY_FORM",
+  }
+}
+
+export const setFormDataForEdit = story => {
+  const storyFormData = {
+    name: story.attributes.name,
+  
+  }
+  return {
+    type: "SET_FORM_DATA_FOR_EDIT",
+    storyFormData
+  }
+}
