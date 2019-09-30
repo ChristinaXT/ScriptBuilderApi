9f6a9777531477f4dcb5635b0ec4d0fc19a95f40
Guess <guess@Guesss-iMac.local>
2019-09-29 21:05:23 -0400

    fixed comments controller error

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
 
     if @comment.save
-      render json: @comment, status: :created, location: @comment
+      render json: @comment, status: :created
     else
       render json: @comment.errors, status: :unprocessable_entity
     end
