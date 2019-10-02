2ae1f12bb2d193dc0e8458f5243be35adb9902c0
Guess <guess@Guesss-iMac.local>
2019-10-01 22:14:49 -0400

    deleted controllers

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,21 @@
 import React from 'react';
-// import '../Home.css';
+import '../home.css';
 import { Link } from 'react-router-dom'
 
 
 const Home = () => {
-    return (
-       <>
-      <h2>Welcome to Script Builder!</h2>
-      <p>This app is meant for novice or professional writers to brainstorm and outline ideas for new stories that can become short stories, plays, teleplays, screenplays or novels</p>
-       <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link>
+  return (
+      <div>
+        <span>
+          <br/>
 
-     </>
-
-  );
-}
-
+            <h2>Welcome to Script Builder!</h2>
+            <p>This app is meant for novice or professional writers to brainstorm and outline ideas for new stories that can become short stories, plays, teleplays, screenplays or novels</p>
+            <br/>
+            <p>Please <Link to="/login">Log in</Link> or <Link to="/signup">Sign up</Link></p>
+         </span>
+      </div>
+    )
+  };
 
 export default Home;
